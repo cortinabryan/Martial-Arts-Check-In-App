bdef9ea818f2eae66d3b462847bf37ac370e93e3
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-07-22 12:34:52 -0700

    added check in button and hover effect

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-// const sched = (document.querySelector("#class-sched").innerHTML = "I changed this via querySelector");
-// const sched = (document.getElementById("class-sched").innerHTML =
-//   "I changed this via getElementById");
+const checkInBtn = document.querySelector(".check-in-btn");
+const checkInBtns = document.querySelectorAll(".check-in-btn");
+const tableRows = document.querySelectorAll(".table-row");
+
+const tableRow = document.querySelectorAll(".table-row");
+
+document.addEventListener("mouseover", function checkHover() {
+  if (tableRow.matches(":hover")) {
+    checkInBtn.style.display = "unset";
+  } else {
+    checkInBtn.style.display = "none";
+  }
+});
+
+// if tr === hover { checkInBtn === display none}
+
+// if (tableRow.matches(":hover")) {
+//   checkInBtn.style.display = "unset";
+// } else {
+//   checkInBtn.style.dispaly = "none";
+// }
+
+// console.log(hovered ? "hovered" : "not hovered");
+// checkHover.hovered = hovered;
+
+// const isHover = (e) => e.parentElement.querySelector(":hover") === e;
+// document.addEventListener("mouseover", function checkHover() {
+//   const hovered = isHover(tableRow);
+//   if (hovered !== checkHover.hovered) {
+//     checkInBtn.style.display = "unset";
+//   } else {
+//     checkInBtn.style.display = "none";
+//   }
+// });
