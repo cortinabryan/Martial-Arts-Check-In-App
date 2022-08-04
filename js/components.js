01d99b2dd37b24d3386d025ffa965af7ab1acd87
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-08-04 10:02:08 -0700

    trying to add delete button at the end table row

--- js/components.js
+++ js/components.js
@@ -1,15 +1,15 @@
 // function that returns the html of the historyTable with the rows inserted!
 const historyTable = (rows) => {
-    return `
+  return `
         <tr>
             <th class="table-header">Class</th>
             <th class="table-header">Date & Time</th>
         </tr>
         ${rows}
       `;
-}
+};
 
 // function that returns the html of historyTable row, two values needed are the name of the class and the time
-const historyTableRow = ({name, time}) => {
-    return `<tr> <td>${name}</td> <td>${time}</td> </tr>`;
-}
+const historyTableRow = ({ name, time }) => {
+  return `<tr> <td>${name}</td> <td>${time}</td> <td><button> Delete </button> </td> </tr>`;
+};
--- js/home.js
+++ js/home.js
@@ -9,50 +9,47 @@ let cumulativeHours = 0;
 
 // represents the data for latest checkin btn clicked!
 let target = {
-  name: '',
-  time: ''
+  name: "",
+  time: "",
 };
 
 // ================================ Functions ==========================================
 // function add history item to localstorage and keeps old state
 const addHistory = () => {
-  const value = [
-    ...(getHistory()),
-    target
-  ]
-  localStorage.setItem('history', JSON.stringify(value));
-}
+  const value = [...getHistory(), target];
+  localStorage.setItem("history", JSON.stringify(value));
+};
 
 // function gets history from localstorage and converts is from json string to actual array
 const getHistory = () => {
-  return JSON.parse(localStorage.getItem('history')) ?? [];
-}
+  return JSON.parse(localStorage.getItem("history")) ?? [];
+};
 
 // function add historyTableRow for each item in the history array
 const historyToRows = () => {
-  return getHistory().map(item => historyTableRow(item)).join('');
-}
+  return getHistory()
+    .map((item) => historyTableRow(item))
+    .join("");
+};
 
 // function adds 2 hours for each history item in the history array
 const historyToHours = () => {
-  return getHistory().reduce((acc) => acc += 2, 0);
-}
+  return getHistory().reduce((acc) => (acc += 2), 0);
+};
 
 const updateHistoryHTML = () => {
-  classHistoryBody.innerHTML = historyTable(
-    historyToRows()
-  );
-}
+  classHistoryBody.innerHTML = historyTable(historyToRows());
+};
 
 const updateHoursHTML = () => {
   beltHoursCur.innerHTML = historyToHours();
-}
+};
 
 // function responsible of handling popup
 const togglePopUp = () => {
   popUpHidden = !popUpHidden;
-  popUpContainer.style.display = popUpHidden ? 'none' : 'flex';
-}
+  popUpContainer.style.display = popUpHidden ? "none" : "flex";
+};
 
 // function to handle all things need to be done when checkin is accepted
 const handleCheckIn = () => {
@@ -63,23 +60,21 @@ const handleCheckIn = () => {
 };
 
 // ================================ Event Handlers ==========================================
-document.getElementById("btn-yes")
-  .addEventListener("click", () => {
-    handleCheckIn();
-  });
+document.getElementById("btn-yes").addEventListener("click", () => {
+  handleCheckIn();
+});
 
-document.getElementById("btn-no")
-  .addEventListener("click", () => {
-    togglePopUp();
-  });
+document.getElementById("btn-no").addEventListener("click", () => {
+  togglePopUp();
+});
 
 checkInBtns.forEach((e) => {
   e.addEventListener("click", (event) => {
     togglePopUp();
     target = {
       name: event.target.getAttribute("data-name"),
-      time: event.target.getAttribute("data-time")
-    }
+      time: event.target.getAttribute("data-time"),
+    };
   });
 });
 
